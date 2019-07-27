ffcd5ad0d7ebd7324b30487132ded8409c55ee0b
Harsh Patel <37637611+HarsH-hP@users.noreply.github.com>
2019-07-28 04:57:33 +0530

    Update Pearson_Correlation_Coefficient.java

--- Pearson_Correlation_Coefficient.java
+++ Pearson_Correlation_Coefficient.java
@@ -7,7 +7,7 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
         Scanner sc=new Scanner(System.in);
         int n=0,count=0;
         do{
